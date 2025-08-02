d48f76b3f8ee74fc1625bfedcd2c0dd8757acf0c
Ferd1xx <zin.andr2004@gmail.com>
2025-08-02 13:56:07 +0300

    hm-1

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.elements['search-text'];
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const query = input.value.trim();
+
+  if (!query) {
+    iziToast.warning({ message: 'Please enter a search term!' });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (err) {
+    iziToast.error({ message: 'An error occurred while fetching images.' });
+  } finally {
+    hideLoader();
+  }
+});
